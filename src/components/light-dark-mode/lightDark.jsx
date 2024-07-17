8e4a8460961527df8b576be9ed1110bafacb56b9
Manish Bagale <manish.bagale3@gmail.com>
2024-07-17 23:28:07 +0545

    created a custom react hook

--- src/components/light-dark-mode/lightDark.jsx
+++ src/components/light-dark-mode/lightDark.jsx
@@ -1,9 +1,19 @@
+import useLocalStorage from "./useLocalStorage";
+
 function LightDarkMode() {
+  const [theme, setTheme] = useLocalStorage("theme", "dark");
+  function handleToggleTheme() {
+    setTheme(theme === "light" ? "dark" : "light");
+  }
+
+  console.log(theme);
   return (
     <>
       <div className="pageContainer">
         <h1>Hellow World!</h1>
-        <button className="changeThemeBtn">Change Theme</button>
+        <button onClick={handleToggleTheme} className="changeThemeBtn">
+          Change Theme
+        </button>
       </div>
     </>
   );
