bddc32d2f4335c5bf925392ed9606fbe0e1af466
Manish Bagale <manish.bagale3@gmail.com>
2024-07-17 22:59:34 +0545

    light and dark mode switching project

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import Accordian from "./components/accordian/accordian.jsx";
 import StarRating from "./components/star-rating/starRating.jsx";
 import ImgSlider from "./components/img-slider/imgSlider.jsx";
 import LoadMore from "./components/load-more-content/loadMore.jsx";
-
+import LightDarkMode from "./components/light-dark-mode/lightDark.jsx";
 function App() {
   return (
     <>
       {/* <Accordian /> */}
       {/* <StarRating /> */}
       {/* <ImgSlider url={"https://picsum.photos/v2/list"} page={1} limit={5} /> */}
-      <LoadMore />
+      {/* <LoadMore /> */}
+      <LightDarkMode />
     </>
   );
 }
---
+++ src/components/light-dark-mode/lightDark.jsx
@@ -0,0 +1,3 @@
+function LightDarkMode() {}
+
+export default LightDarkMode;
