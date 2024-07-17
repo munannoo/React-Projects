2aa47711e158dafaeabf3d143da5a23fc74fbcf8
Manish Bagale <manish.bagale3@gmail.com>
2024-07-17 23:17:58 +0545

    added some html and css styles

--- src/components/light-dark-mode/lightDark.jsx
+++ src/components/light-dark-mode/lightDark.jsx
@@ -1,3 +1,12 @@
-function LightDarkMode() {}
+function LightDarkMode() {
+  return (
+    <>
+      <div className="pageContainer">
+        <h1>Hellow World!</h1>
+        <button className="changeThemeBtn">Change Theme</button>
+      </div>
+    </>
+  );
+}
 
 export default LightDarkMode;
