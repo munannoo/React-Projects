e69447563d32427feb0f31a1fa597249fa80d121
Manish Bagale <manish.bagale3@gmail.com>
2024-07-21 23:23:35 +0545

    added styles

--- src/components/modal-popup/popup.jsx
+++ src/components/modal-popup/popup.jsx
@@ -1,8 +1,8 @@
 import "../styles.css";
 export default function PopUp({ title, content, footer, onClose }) {
   return (
-    <div className="container">
-      <button onClick={onClose}>X</button>
+    <div className="modal-container">
+      <button onClick={onClose}>x</button>
       <h1 className="title">{title ? title : "this is title"}</h1>
       <p className="content">{content ? content : "this is the body"}</p>
       <p className="footer">{footer ? footer : "this is the footer"}</p>
