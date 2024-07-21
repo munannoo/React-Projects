9f3a818571f29db6d6cab7ff08ab6fbc01efc642
Manish Bagale <manish.bagale3@gmail.com>
2024-07-21 23:15:06 +0545

    added all functionality

--- src/components/modal-popup/modal.jsx
+++ src/components/modal-popup/modal.jsx
@@ -1,16 +1,22 @@
 import { useState } from "react";
 import PopUp from "./popup";
 
-function handleToggleModal() {
-  setShowModal(true);
-}
-
 export default function ModalPopUp() {
   const [showModal, setShowModal] = useState(false);
+
+  function handleToggleModal() {
+    setShowModal(true);
+  }
+  function handleOnClose() {
+    console.log(showModal);
+    setShowModal(false);
+  }
   return (
     <div>
-      <button onClick={handleToggleModal}>Open PopUp!</button>
-      {showModal ? <PopUp /> : null}
+      <button onClick={() => handleToggleModal()}>Open PopUp!</button>
+      {showModal ? (
+        <PopUp title={"MODAL POPUP!"} onClose={handleOnClose} />
+      ) : null}
     </div>
   );
 }
--- src/components/modal-popup/popup.jsx
+++ src/components/modal-popup/popup.jsx
@@ -1,1 +1,11 @@
-export default function PopUp() {}
+import "../styles.css";
+export default function PopUp({ title, content, footer, onClose }) {
+  return (
+    <div className="container">
+      <button onClick={onClose}>X</button>
+      <h1 className="title">{title ? title : "this is title"}</h1>
+      <p className="content">{content ? content : "this is the body"}</p>
+      <p className="footer">{footer ? footer : "this is the footer"}</p>
+    </div>
+  );
+}
