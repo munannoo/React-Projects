4e63b9babcef1bc28b046fde757096fbfe54ff0a
Manish Bagale <manish.bagale3@gmail.com>
2024-07-25 00:04:32 +0545

    made MenuItem show the item form data and added recurrsion

--- src/components/tree-view/menu-item.jsx
+++ src/components/tree-view/menu-item.jsx
@@ -1,3 +1,16 @@
+import { useState } from "react";
+import MenuList from "./menu-list";
+
 export default function MenuItem({ item }) {
-  return <div className="menuItem">hello</div>;
+  const [displayCurrentChildren, setDisplay] = useState({});
+  console.log(item);
+  return (
+    <li className="menuItem">
+      <p>{item.label}</p>
+
+      {item && item.children && item.children.length > 0 ? (
+        <MenuList list={item.children} />
+      ) : null}
+    </li>
+  );
 }
