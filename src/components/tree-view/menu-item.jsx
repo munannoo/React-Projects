31b80fb92a2c7766074b5a68a9ad60c9d402ac96
Manish Bagale <manish.bagale3@gmail.com>
2024-07-25 12:21:06 +0545

    made drop show show only on click

--- src/components/tree-view/menu-item.jsx
+++ src/components/tree-view/menu-item.jsx
@@ -2,13 +2,27 @@ import { useState } from "react";
 import MenuList from "./menu-list";
 
 export default function MenuItem({ item }) {
-  const [displayCurrentChildren, setDisplay] = useState({});
-  console.log(item);
+  const [displayChildren, setDisplayChildren] = useState({});
+
+  function handleToggleShowChildren(itemLabel) {
+    setDisplayChildren({
+      ...displayChildren,
+      [itemLabel]: !displayChildren[itemLabel],
+    });
+  }
+
   return (
     <li className="menuItem">
-      <p>{item.label}</p>
-
-      {item && item.children && item.children.length > 0 ? (
+      <div style={{ display: "flex", gap: "1rem" }}>
+        <p>{item.label}</p>
+        {item && item.children && item.children.length ? (
+          <p onClick={() => handleToggleShowChildren(item.label)}>+</p>
+        ) : null}
+      </div>
+      {item &&
+      item.children &&
+      item.children.length > 0 &&
+      displayChildren[item.label] ? (
         <MenuList list={item.children} />
       ) : null}
     </li>
--- src/components/tree-view/menu-list.jsx
+++ src/components/tree-view/menu-list.jsx
@@ -2,12 +2,12 @@ import MenuItem from "./menu-item.jsx";
 
 export default function MenuList({ list = [] }) {
   return (
-    <div className="menuListContainer">
+    <ul className="menuListContainer">
       {list && list.length
         ? list.map((listItem, index) => (
             <MenuItem key={index} item={listItem} />
           ))
         : console.log("not present!")}
-    </div>
+    </ul>
   );
 }
