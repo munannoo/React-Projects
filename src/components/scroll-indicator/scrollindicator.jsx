609b363721be7d9794041c88a0927295547197e7
Manish Bagale <manish.bagale3@gmail.com>
2024-07-26 23:32:59 +0545

    dispalyed fetched data

--- src/components/scroll-indicator/scrollindicator.jsx
+++ src/components/scroll-indicator/scrollindicator.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 export default function ScrollIndicator({ url }) {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
-  const [data, setData] = useState([]);
+  const [displayData, setDisplayData] = useState([]);
 
   async function handleFetchData(getUrl) {
     try {
@@ -12,7 +12,11 @@ export default function ScrollIndicator({ url }) {
       const response = await fetch(getUrl);
       const data = await response.json();
 
-      console.log(data);
+      if (data && data.products && data.products.length > 0) {
+        setLoading(false);
+        setDisplayData(data.products);
+        console.log(data, displayData);
+      }
     } catch (e) {
       setError(e.error);
       console.log(e.error);
@@ -25,7 +29,13 @@ export default function ScrollIndicator({ url }) {
 
   return (
     <div className="scrollContainer">
+      {loading ? <div>loading data pls wait!</div> : null}
       <h1>SCROLL INDICATOR</h1>
+      <div className="dataContainer">
+        {displayData && displayData.length > 0
+          ? displayData.map((dataItem) => <p>{dataItem.title}</p>)
+          : null}
+      </div>
     </div>
   );
 }
