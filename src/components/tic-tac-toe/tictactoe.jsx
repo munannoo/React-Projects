6afc72bbec6056ee9ea944043d066b704259807b
Manish Bagale <manish.bagale3@gmail.com>
2024-07-29 15:08:01 +0545

    added functionality to check winner

--- src/components/tic-tac-toe/tictactoe.jsx
+++ src/components/tic-tac-toe/tictactoe.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../styles.css";
+import { WiNightLightning } from "react-icons/wi";
 
 export default function TicTacToe() {
   const [isOtrun, setisOtrun] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(""));
+  const [status, setStatus] = useState("");
 
   function Squares({ value, onclick }) {
     return (
@@ -13,11 +15,48 @@ export default function TicTacToe() {
     );
   }
 
+  useEffect(() => {
+    if (!getWinner(squares) && squares.every((item) => item !== "")) {
+      setStatus("This is a draw");
+    } else if (getWinner(squares)) {
+      setStatus(`The winner is ${getWinner(squares)}`);
+    } else {
+      setStatus(`Its ${isOtrun ? "O" : "X"}'s turn`);
+    }
+  }, [squares, isOtrun]);
+
+  function getWinner() {
+    let winPattern = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < winPattern.length; i++) {
+      const [x, y, z] = winPattern[i];
+
+      if (
+        squares[x] &&
+        squares[x] === squares[y] &&
+        squares[x] === squares[z]
+      ) {
+        return squares[x];
+      }
+    }
+    return null;
+  }
+
   function handleOnClick(getSqaure) {
     let cpySquare = [...squares];
-    if (cpySquare[getSqaure]) return;
+    if (getWinner(cpySquare[getSqaure]) || cpySquare[getSqaure]) return;
     cpySquare[getSqaure] = isOtrun ? "O" : "X";
     setisOtrun(!isOtrun);
+    console.log(isOtrun);
     setSquares(cpySquare);
   }
 
@@ -38,6 +77,8 @@ export default function TicTacToe() {
         <Squares value={squares[7]} onclick={() => handleOnClick(7)} />
         <Squares value={squares[8]} onclick={() => handleOnClick(8)} />
       </div>
+
+      <h1>{status}</h1>
     </div>
   );
 }
