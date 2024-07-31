5e9d70a0a06e0691a05a09a4546f27e94e23ad2c
Manish Bagale <manish.bagale3@gmail.com>
2024-07-31 23:58:57 +0545

    displayed fetched data

--- src/components/useFetch-hook/test.jsx
+++ src/components/useFetch-hook/test.jsx
@@ -6,11 +6,18 @@ export default function UseFetchHookTest() {
     {}
   );
 
+  console.log(data);
+
   return (
     <div>
       <h1>Custom useFetch Hook</h1>
       {loading ? <h3>pending data pls wait....</h3> : null}
       {error ? <h3>an error has occured!!</h3> : null}
+      {data && data.products && data.products.length > 0
+        ? data.products.map((dataItem) => {
+            return <p>{dataItem.title}</p>;
+          })
+        : null}
     </div>
   );
 }
