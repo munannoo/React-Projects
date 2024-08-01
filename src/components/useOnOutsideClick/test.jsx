83a256f3e9e185ee67538bfa68f6ac92eedcd825
Manish Bagale <manish.bagale3@gmail.com>
2024-08-01 23:20:53 +0545

    imported modal from previous project

--- src/components/useOnOutsideClick/test.jsx
+++ src/components/useOnOutsideClick/test.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+import PopUp from "../modal-popup/popup";
+
 export default function TestOutsideClick() {
+  const [showModal, setShowModal] = useState(false);
+
+  function handleToggleModal() {
+    setShowModal(true);
+  }
+
+  function handleOnClose() {
+    setShowModal(false);
+  }
+
   return (
     <div>
       <h1>OnOutsideClick Hook React</h1>
+      <button onClick={() => handleToggleModal()}>Open PopUp!</button>
+      {showModal ? (
+        <PopUp title={"MODAL POPUP!"} onClose={handleOnClose} />
+      ) : null}
     </div>
   );
 }
