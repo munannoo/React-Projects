e14c2706059c1eb92953565a6cefb57fbee255f4
Manish Bagale <manish.bagale3@gmail.com>
2024-08-01 23:28:36 +0545

    added functionality to the custom hook

--- src/components/useOnOutsideClick/useOnOutsideclick.jsx
+++ src/components/useOnOutsideClick/useOnOutsideclick.jsx
@@ -1,1 +1,20 @@
-export default function useOnOutsideClick() {}
+import { useEffect } from "react";
+
+export default function useOnOutsideClick(ref, handler) {
+  useEffect(() => {
+    function listener(event) {
+      if (!ref.current || ref.current(event.target)) {
+        return;
+      }
+      handler(event);
+    }
+
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [handler, ref]);
+}
