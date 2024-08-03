3293fe75848fb8021ce610ea62632e03e2f88d41
Manish Bagale <manish.bagale3@gmail.com>
2024-08-03 18:22:36 +0545

    added code to check width and height

--- src/components/useWindowResize/test.jsx
+++ src/components/useWindowResize/test.jsx
@@ -1,2 +1,13 @@
 import useWindowResize from "./useWindowResize";
-export default function TestUseWindowResize() {}
+
+export default function TestUseWindowResize() {
+  const windowSize = useWindowResize();
+  const { width, height } = windowSize;
+  return (
+    <div>
+      <h1>useWindowResize Hook</h1>
+      <p>width is {width}</p>
+      <p>height is {height}</p>
+    </div>
+  );
+}
