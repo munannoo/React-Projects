84f43a4b9cc9b688c3c9a392ffc7f6f54ac18959
Manish Bagale <manish.bagale3@gmail.com>
2024-08-04 23:44:19 +0545

    added buttons and functionality to scroll to top and bottom of the page

--- src/components/scroll-top-bottom/scrollTopBottom.jsx
+++ src/components/scroll-top-bottom/scrollTopBottom.jsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import useFetch from "../useFetch-hook/useFetch";
 
 export default function ScrollTopBottom() {
@@ -5,6 +6,8 @@ export default function ScrollTopBottom() {
     "https://dummyjson.com/products?limit=100",
     {}
   );
+
+  const bottomRef = useRef(null);
 
   if (pending) {
     return <h3>loading data pls wait...</h3>;
@@ -14,14 +17,30 @@ export default function ScrollTopBottom() {
     return <h3>an error has occured!</h3>;
   }
 
+  function handleToBottom() {
+    bottomRef.current.scrollIntoView({ behavior: "smooth" });
+  }
+  function handleToTop() {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  }
+
+  console.log(bottomRef);
+
   return (
     <div>
       <h1>Scroll from Top to Bottom</h1>
       <h3>this is the top section</h3>
+      <button onClick={handleToBottom}>to bottom</button>
       {data && data.products && data.products.length
         ? data.products.map((dataItem) => <p>{dataItem.title}</p>)
         : null}
       <h3>this is the bottom section</h3>
+      <div ref={bottomRef}></div>
+      <button onClick={handleToTop}>to top</button>
     </div>
   );
 }
