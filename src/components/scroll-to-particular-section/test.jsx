2a59a2b77e3a5acd7c677540b699f18fd5fda094
Manish Bagale <manish.bagale3@gmail.com>
2024-08-05 23:55:35 +0545

    created a button to scroll to the page 3

--- src/components/scroll-to-particular-section/test.jsx
+++ src/components/scroll-to-particular-section/test.jsx
@@ -1,4 +1,7 @@
+import { useRef } from "react";
+
 export default function ScrollToParticularSection() {
+  const ref = useRef();
   const pagesData = [
     {
       label: "First Page",
@@ -33,11 +36,22 @@ export default function ScrollToParticularSection() {
       },
     },
   ];
+
+  function handleScrolltoSecion() {
+    let pos = ref.current.getBoundingClientRect().top;
+
+    window.scrollTo({
+      top: pos,
+      behavior: "smooth",
+    });
+  }
+
   return (
     <div>
       <h1>Scroll to a Particular Section</h1>
-      {pagesData.map((pageItem) => (
-        <div style={pageItem.style}>
+      <button onClick={handleScrolltoSecion}>to section 3</button>
+      {pagesData.map((pageItem, index) => (
+        <div ref={index + 1 === 3 ? ref : null} style={pageItem.style}>
           <h2>{pageItem.label}</h2>
         </div>
       ))}
