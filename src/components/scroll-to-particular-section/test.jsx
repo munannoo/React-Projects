d9f951335f2afff3931d1a37bcc2803c8d678805
Manish Bagale <manish.bagale3@gmail.com>
2024-08-05 23:48:19 +0545

    created a data for pages and displayed them

--- src/components/scroll-to-particular-section/test.jsx
+++ src/components/scroll-to-particular-section/test.jsx
@@ -1,3 +1,46 @@
 export default function ScrollToParticularSection() {
-  return <div></div>;
+  const pagesData = [
+    {
+      label: "First Page",
+      style: {
+        width: "100%",
+        height: "600px",
+        background: "red",
+      },
+    },
+    {
+      label: "Second Page",
+      style: {
+        width: "100%",
+        height: "600px",
+        background: "green",
+      },
+    },
+    {
+      label: "Third Page",
+      style: {
+        width: "100%",
+        height: "600px",
+        background: "blue",
+      },
+    },
+    {
+      label: "Fourth Page",
+      style: {
+        width: "100%",
+        height: "600px",
+        background: "purple",
+      },
+    },
+  ];
+  return (
+    <div>
+      <h1>Scroll to a Particular Section</h1>
+      {pagesData.map((pageItem) => (
+        <div style={pageItem.style}>
+          <h2>{pageItem.label}</h2>
+        </div>
+      ))}
+    </div>
+  );
 }
