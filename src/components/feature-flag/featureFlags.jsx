1d5f650236875b8dcf508d5e0474a19457bcdaca
Manish Bagale <manish.bagale3@gmail.com>
2024-08-09 00:00:59 +0545

    created data of components to render

--- src/components/feature-flag/featureFlags.jsx
+++ src/components/feature-flag/featureFlags.jsx
@@ -1,4 +1,39 @@
+import { useContext } from "react";
+import Accordian from "../accordian/accordian";
+import LightDarkMode from "../light-dark-mode/lightDark";
+import StarRating from "../star-rating/starRating";
+import Tabs from "../tabs/tabs";
+import TicTacToe from "../tic-tac-toe/tictactoe";
+import { FeatureFlagContext } from ".";
+
 export default function FeatureFlags() {
+  const { loading, enabledFlags } = useContext(FeatureFlagContext);
+  const componentsToRender = [
+    {
+      key: "showLightAndDarkMode",
+      component: <LightDarkMode />,
+    },
+    {
+      key: "showTicTacToe",
+      component: <TicTacToe />,
+    },
+    {
+      key: "showAccordian",
+      component: <Accordian />,
+    },
+    {
+      key: "showTabs",
+      component: <Tabs />,
+    },
+    {
+      key: "showStarRating",
+      component: <StarRating />,
+    },
+  ];
+  if (loading) {
+    return <h1>loading data pls wait...</h1>;
+  }
+
   return (
     <div>
       <h1>Feature Flags!</h1>
