df34b8a41b8e0159546753095413f26fa0b941cc
Manish Bagale <manish.bagale3@gmail.com>
2024-08-09 00:00:08 +0545

    fetched data from data service call

--- src/components/feature-flag/index.jsx
+++ src/components/feature-flag/index.jsx
@@ -1,10 +1,30 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import featureFlagDataServiceCall from "./data";
 
 export const FeatureFlagContext = createContext(null);
 
 export default function FeatureFlagGlobalState({ children }) {
+  const [loading, setLoading] = useState(false);
+  const [enabledFlags, setEnabledFlags] = useState({});
+
+  async function fetchFeatureFlags() {
+    try {
+      setLoading(true);
+      const response = await featureFlagDataServiceCall();
+      setEnabledFlags(response);
+      setLoading(false);
+    } catch (e) {
+      setLoading(false);
+      console.log(e.error);
+      throw new Error(e.error);
+    }
+  }
+
+  useEffect(() => {
+    fetchFeatureFlags();
+  });
   return (
-    <FeatureFlagContext.Provider value={{}}>
+    <FeatureFlagContext.Provider value={{ enabledFlags }}>
       {children}
     </FeatureFlagContext.Provider>
   );
